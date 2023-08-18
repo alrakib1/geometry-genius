92098c947046e50b61a9864e37fae5fa926021d4
alrakib1 <abdullahalrakib30@gmail.com>
2023-08-18 21:49:07 +0600

    added rhombus

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,29 +1,53 @@
 // triangle calculator
 function calculateTriangleArea() {
   const base = getInputValue("triangle-base");
-
   const height = getInputValue("triangle-height");
-
+  if (isNaN(base) || isNaN(height)) {
+    alert("Please add number");
+    return;
+  }
   const area = 0.5 * height * base;
   setElementInnerText("triangle-area", area);
+  addToCalculationEntry('Triangle',area);
 }
 // Rectangle Calculator
 function calculateRectangleArea() {
   const width = getInputValue("rectangle-width");
   const length = getInputValue("rectangle-length");
+  if (isNaN(width) || isNaN(length)) {
+    alert("Please add number");
+    return;
+  }
   const area = width * length;
   setElementInnerText("rectangle-area", area);
+  addToCalculationEntry('Rectangle',area);
 }
+// Parallelogram Calculator
 function calculateParallelogramArea() {
   const base = getInputValue("parallelogram-base");
   const height = getInputValue("parallelogram-height");
+  if (isNaN(base) || isNaN(height)) {
+    alert("Please insert number");
+    return;
+  }
   const area = base * height;
   setElementInnerText("Parallelogram-area", area);
+  // add to calculation entry
+  addToCalculationEntry('Parallelogram',area);
 }
+// Ellipse Calculator
+function calculateEllipseArea() {
+  const majorRadius = getInputValue("ellipse-major-radius");
+  const minorRadius = getInputValue("ellipse-minor-radius");
+  if (isNaN(majorRadius) || isNaN(minorRadius)) {
+    alert("Please insert number");
+    return;
+  }
+  const area = (3.14 * majorRadius * minorRadius).toFixed(2);
+  setElementInnerText("ellipse-area", area);
+  addToCalculationEntry('Parallelogram',area);
+}
+// Data validation
+// set the proper type of input field
+// check type
 
-function calculateEllipseArea(){
-    const majorRadius = getInputValue("ellipse-major-radius");
-    const minorRadius = getInputValue('ellipse-minor-radius');
-    const area = 3.14 * majorRadius * minorRadius;
-    setElementInnerText('ellipse-area', area)
-}
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -2,12 +2,32 @@
 function getInputValue(fieldId) {
   const inputField = document.getElementById(fieldId);
   const inputValueText = inputField.value;
-  const value = parseFloat(inputValueText);
-  inputField.value = "";
-  return value;
-}
+    const value = parseFloat(inputValueText);
+    inputField.value = "";
+    return value;
+  }
+
 // reusable set innerText
 function setElementInnerText(elementId, area) {
   const element = document.getElementById(elementId);
   element.innerText = area;
+}
+
+// calculation entry
+// get the element where you want to add the dynamic html
+// create an element you want to add
+// if needed add some class
+// set inner html it could be dynamic string
+// append child
+function addToCalculationEntry(areaType, area){
+  console.log(areaType + ' '+ area)
+  const calculationEntry = document.getElementById('calculation-entry');
+  const count = calculationEntry.childElementCount;
+  
+  const p = document.createElement("p");
+  p.classList.add('my-4');
+  // p.innerHTML =areaType + ' ' + area;
+  p.innerHTML =`${count +1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">convert</button>`;
+  calculationEntry.appendChild(p);
+
 }
